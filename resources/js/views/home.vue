f00196160b991eb7960c38d45d6b00866d2938ff
Febri Hidayan <febrihidayan20@gmail.com>
2020-05-28 20:17:19 +0700

    perbarui home

--- resources/js/views/home.vue
+++ resources/js/views/home.vue
@@ -1,3 +1,22 @@
 <template>
-    
+    <div class="hero has-text-centered">
+        <div class="hero-body">
+            <h1 class="title is-1">Laravel Sanctum Vue</h1>
+            <p class="subtitle">Template dasar untuk membangun projek Laravel Sanctum SPA.</p>
+            <p>
+                <a href="//packagist.org/packages/hilyahtech/laravel-sanctum-vue" rel="nofollow">
+                    <img src="https://camo.githubusercontent.com/95635c63d1d443e66144d8ddd672ae74135f68f0/68747470733a2f2f706f7365722e707567782e6f72672f68696c796168746563682f6c61726176656c2d73616e6374756d2d7675652f76" alt="Latest Stable Version" data-canonical-src="https://poser.pugx.org/hilyahtech/laravel-sanctum-vue/v" style="max-width:100%;">
+                </a>
+                <a href="//packagist.org/packages/hilyahtech/laravel-sanctum-vue" rel="nofollow">
+                    <img src="https://camo.githubusercontent.com/a7e1403757be39d6e47aa198775afb4caee3678d/68747470733a2f2f706f7365722e707567782e6f72672f68696c796168746563682f6c61726176656c2d73616e6374756d2d7675652f646f776e6c6f616473" alt="Total Downloads" data-canonical-src="https://poser.pugx.org/hilyahtech/laravel-sanctum-vue/downloads" style="max-width:100%;">
+                </a>
+                <a href="//packagist.org/packages/hilyahtech/laravel-sanctum-vue" rel="nofollow">
+                    <img src="https://camo.githubusercontent.com/bc2c76200947cb73e16d51d277e02018d37eacf2/68747470733a2f2f706f7365722e707567782e6f72672f68696c796168746563682f6c61726176656c2d73616e6374756d2d7675652f762f756e737461626c65" alt="Latest Unstable Version" data-canonical-src="https://poser.pugx.org/hilyahtech/laravel-sanctum-vue/v/unstable" style="max-width:100%;">
+                </a>
+                <a href="//packagist.org/packages/hilyahtech/laravel-sanctum-vue" rel="nofollow">
+                    <img src="https://camo.githubusercontent.com/2a9f811f1aa7366ed18cd56bf2979bb40c5a171f/68747470733a2f2f706f7365722e707567782e6f72672f68696c796168746563682f6c61726176656c2d73616e6374756d2d7675652f6c6963656e7365" alt="License" data-canonical-src="https://poser.pugx.org/hilyahtech/laravel-sanctum-vue/license" style="max-width:100%;">
+                </a>
+            </p>
+        </div>
+    </div>
 </template>
