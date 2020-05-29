ba79beca59e395caa2780c7622985eb2c8bcd612
Febri Hidayan <febrihidayan20@gmail.com>
2020-05-29 14:21:30 +0700

    perbarui routing api

--- routes/spa/api.php
+++ routes/spa/api.php
@@ -8,11 +8,24 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::group(['middleware' => 'auth:sanctum', 'namespace' => 'Auth'], function() {
+
     Route::get('user', function() {
         return response()->json(Auth::user());
     });
+
+    Route::post('logout', 'LoginController@logout');
+
 });
 
 Route::group(['middleware' => 'guest:api', 'namespace' => 'Auth'], function() {
+
     Route::post('login', 'loginController@login');
+    Route::post('register', 'RegisterController@register');
+
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/reset', 'ResetPasswordController@reset');
+
+    Route::post('email/verify/{user}', 'VerificationController@verify')->name('verification.verify');
+    Route::post('email/resend', 'VerificationController@resend');
+
 });
